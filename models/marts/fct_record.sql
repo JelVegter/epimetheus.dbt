7947dff52f3ac8bd66275c57b6aa14d3f8f368a7
Jelle Vegter <jelle.vegter@nederlandseloterij.nl>
2022-05-28 12:30:26 +0200

    moved filter to first select

--- models/marts/fct_record.sql
+++ models/marts/fct_record.sql
@@ -9,15 +9,16 @@ WITH source_data AS (
         record, table_name, transaction_dt, extraction_dt, write_dt
     FROM
         {{ ref('sampled_record') }}
+    WHERE
+        {{ dbt_utils.datediff("write_dt", "getdate()", 'day') }} < 15
 )
 SELECT
     {{ dbt_utils.surrogate_key(['record','table_name','write_dt']) }} AS record_id,
-    *,
+    record, table_name, transaction_dt, extraction_dt, write_dt,
     {{ dbt_date.now() }} as _write_ts,
     {{ dbt_date.today() }} as _write_date
 FROM
     source_data
-WHERE
-{{ dbt_utils.datediff("write_dt", "getdate()", 'day') }} < 15
+
 
 
