9ef81a7548f1ec9f2732af10761e4588dcf8436b
Jelle Vegter <jelle.vegter@nederlandseloterij.nl>
2022-05-28 12:22:39 +0200

    added _date

--- models/stg/correctness_check.sql
+++ models/stg/correctness_check.sql
@@ -5,13 +5,13 @@
 WITH source_data AS (
 
     SELECT
-        daily_check, caerus_id, record_id
+        daily_check, caerus_id, record_id, _date
     FROM
         dbo.fact_check
     WHERE daily_check != 'bets_without_profile'
 )
 SELECT
-    {{ dbt_utils.surrogate_key(['daily_check','caerus_id','{{ dbt_date.now() }} as _write_date']) }} AS id,
+    {{ dbt_utils.surrogate_key(['daily_check','caerus_id', '_date']) }} AS id,
     *,
     {{ dbt_date.now() }} as _write_ts,
     {{ dbt_date.today() }} as _write_date
