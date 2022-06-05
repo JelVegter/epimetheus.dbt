d57e50bc0add110130c2775d7e9cb8f78921005d
Jelle Vegter <jelle.vegter@nederlandseloterij.nl>
2022-06-05 16:35:49 +0200

    added dim_zip

---
+++ models/marts/dim_zip.sql
@@ -0,0 +1,25 @@
+{{ config(
+    materialized = 'table'
+) }}
+--
+WITH source_data AS (
+
+    SELECT
+        {% if target.name == 'dev' %}
+            TOP 1000
+        {% endif %}
+        datasafe_id,
+        blob_name,
+        filename
+    FROM
+        {{ source('epimetheus', 'sent_zip')}}
+)
+SELECT
+    {{ dbt_utils.surrogate_key(['filename','generation_date']) }} AS zip_id,
+    datasafe_id,
+    blob_name,
+    filename,
+    {{ dbt_date.now() }} as _ts
+FROM
+    source_data
+
